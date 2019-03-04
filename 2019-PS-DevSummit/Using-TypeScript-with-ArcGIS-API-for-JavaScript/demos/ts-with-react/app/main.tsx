f4ef9289800c413d6ed8ac9b4f9d63c4e2f5b93f
Stefan Eilemann <SEilemann@esri.com>
2019-03-04 19:12:35 +0100

    remove misplaced pres

--- 2019-PS-DevSummit/Using-TypeScript-with-ArcGIS-API-for-JavaScript/demos/ts-with-react/app/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import AppBody from "./components/AppBody";
-
-ReactDOM.render(
-  <AppBody />,
-  document.getElementById("viewDiv")
-);
